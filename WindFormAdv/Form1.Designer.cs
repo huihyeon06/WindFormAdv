5858fd30ee7eed645c7512ee21cfa1a9731ae356
Huihyeon Park <agnea0829@gmail.com>
2024-08-22 11:49:03 +0900

    도움말>도움말 보기 구현

--- WindFormAdv/Form1.Designer.cs
+++ WindFormAdv/Form1.Designer.cs
@@ -163,6 +163,7 @@
             this.끝내기ToolStripMenuItem.Name = "끝내기ToolStripMenuItem";
             this.끝내기ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.끝내기ToolStripMenuItem.Text = "끝내기";
+            this.끝내기ToolStripMenuItem.Click += new System.EventHandler(this.끝내기ToolStripMenuItem_Click);
             // 
             // 편집ToolStripMenuItem
             // 
@@ -339,6 +340,7 @@
             this.도움말보기ToolStripMenuItem.Name = "도움말보기ToolStripMenuItem";
             this.도움말보기ToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
             this.도움말보기ToolStripMenuItem.Text = "도움말 보기";
+            this.도움말보기ToolStripMenuItem.Click += new System.EventHandler(this.도움말보기ToolStripMenuItem_Click);
             // 
             // 피드백보내ToolStripMenuItem
             // 
